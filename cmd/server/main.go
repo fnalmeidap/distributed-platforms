14aa3065d32398de1e719b5cdc7e79a7b7e6ff5e
fnalmeidap <fnap@cin.ufpe.br>
2024-10-30 00:19:42 -0300

    Remove duplicate srh

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -3,18 +3,12 @@ package main
 import (
 	calculator "distributed-platforms/internal/app/calculator"
 	messaginginvoker "distributed-platforms/internal/distribution/invoker"
-	srh "distributed-platforms/internal/infra/srh"
 	lease "distributed-platforms/internal/lease"
 	shared "distributed-platforms/internal/shared"
-	"fmt"
 )
 
-func StartServer(srh *srh.SRH, inv messaginginvoker.Invoker) {
-
-	fmt.Println("Server listening on", srh.Host)
-
+func StartServer(inv messaginginvoker.Invoker) {
 	inv.Invoke()
-
 }
 
 func main() {
@@ -23,8 +17,6 @@ func main() {
 	leaseManager := &lease.LeaseManager{Leases: make(map[string]lease.Lease)}
 	app := &calculator.Calculator{}
 	invoker := messaginginvoker.NewInvoker(ior.Host, ior.Port, app, leaseManager)
-	SRH := srh.NewSRH(ior.Host, ior.Port)
-	//handler := NewServiceRequestHandler(invoker)
-	StartServer(SRH, invoker)
 
+	StartServer(invoker)
 }
